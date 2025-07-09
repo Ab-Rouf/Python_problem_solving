c242518c4fae62d1225f36e2017c19d159aff174
Abdur Rouf <114940048+Ab-Rouf@users.noreply.github.com>
2025-07-09 14:46:22 +0600

    Update problem-3.py

--- Beginner-Level/Problem-3/problem-3.py
+++ Beginner-Level/Problem-3/problem-3.py
@@ -1,1 +1,12 @@
+#find duplicate data in a list
+l1=["ai", "ml", "python", "ml", "dl", "ai"]
+unique=set()
+duplicate=set()
 
+for element in l1:
+    if element not in unique:
+        unique.add(element)
+    elif element not in duplicate:
+        duplicate.add(element)
+dup=list(duplicate)
+print(dup)
