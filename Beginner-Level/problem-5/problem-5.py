a74a846821ef43fe970eaa5243fd7a3ee5d7e8d3
Abdur Rouf <114940048+Ab-Rouf@users.noreply.github.com>
2025-07-12 23:11:36 +0600

    Update problem-5.py

--- Beginner-Level/problem-5/problem-5.py
+++ Beginner-Level/problem-5/problem-5.py
@@ -1,1 +1,10 @@
-
+def flatten_list(nested_list):
+  f_list=[]
+  for elements in nested_list:
+    if isinstance(elements,list):
+      f_list.extend(flatten_list(elements))
+    else:
+      f_list.append(elements)
+  return f_list
+list1=[1, [2, 3], [4, [5]]]
+print(flatten_list(list1))
