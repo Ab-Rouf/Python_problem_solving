7fe8c1ccd249dd23b048bfb40c489d653465b028
Abdur Rouf <114940048+Ab-Rouf@users.noreply.github.com>
2025-07-13 00:16:32 +0600

    Update problem-6.py

--- Beginner-Level/problem-6/problem-6.py
+++ Beginner-Level/problem-6/problem-6.py
@@ -1,1 +1,10 @@
+def capitalization(text):
+  words=text.split()
+  capitalization_words=[]
+  for word in words:
+    capitalization_words.append(word.capitalize())
+  return ' '.join(capitalization_words)
+  
 
+text=input("Enter your sentences:")
+print(capitalization(text))
