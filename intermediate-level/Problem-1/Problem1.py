60502cf7c195e301ddeac43579421311818d7e5e
Abdur Rouf <114940048+Ab-Rouf@users.noreply.github.com>
2025-07-14 13:36:16 +0600

    Update Problem1.py

--- intermediate-level/Problem-1/Problem1.py
+++ intermediate-level/Problem-1/Problem1.py
@@ -1,1 +1,12 @@
+# Problem-1: Longest Word in a Sentence
+def longest_word(string):
+  words=string.split()
+  longest_word=max(words,key=len)
+  return longest_word
+
+
+s=input("Enter your sentences:")
+
+print(longest_word(s))
+
 
