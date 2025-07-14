33525c0b8ec8f34aedd301629ccd663c383c895f
Abdur Rouf <114940048+Ab-Rouf@users.noreply.github.com>
2025-07-14 20:55:39 +0600

    Update Problem2.py

--- intermediate-level/Problem-2/Problem2.py
+++ intermediate-level/Problem-2/Problem2.py
@@ -1,1 +1,14 @@
+# Problem-2: Group Anagrams Together
+def Anagrams(words):
+    anagrams_dict = {}
+    for word in words:
+        key = ''.join(sorted(word))
+        if key in anagrams_dict:
+            anagrams_dict[key].append(word)
+        else:
+            anagrams_dict[key] = [word]
+    return list(anagrams_dict.values())
 
+
+input_words = ["bat", "tab", "cat", "act"]
+print(Anagrams(input_words))
